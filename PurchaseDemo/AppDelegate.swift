354d78dd105a40c02ccb99c41054b5064c56bec9
Henry.Liao <Henry.Liao@skylinetw.com>
2021-07-29 11:03:03 +0800

    feat(AppDelegate)：監聽paymentQueue交易更新

--- PurchaseDemo/AppDelegate.swift
+++ PurchaseDemo/AppDelegate.swift
@@ -34,9 +34,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, SKPaymentTransactionObser
     }
     
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-        
+        for transaction in transactions {
+             switch transaction.transactionState {
+             case .purchasing:
+                print("purchasing...")
+             case .deferred:
+                print("deferred")
+             case .failed:
+                print("failed")
+             case .purchased:
+                print("purchased")
+                SKPaymentQueue.default().finishTransaction(transaction)
+             case .restored:
+                print("restored")
+             @unknown default: print("Unexpected transaction state \(transaction.transactionState)")
+            }
+        }
     }
-
-
 }
 
